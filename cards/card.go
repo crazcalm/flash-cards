b8be710cf5b1d0af921160ef0e60d548f0766387
Marcus Willock <crazcalm@gmail.com>
2017-09-15 06:10:07 -0400

    Fixing flipped issue
    I reported an issue with the cards not being able to flip back and forth.
    I added a flipped property to the Card struct and a flip method to control the
    flipped state. I also needed to pass in a pointer of the card to one of the template
    functions so that it can modify the card

--- cards/card.go
+++ cards/card.go
@@ -2,7 +2,17 @@ package flashcards
 
 //Card is a struct to represent a single flash card
 type Card struct {
-	Front string `csv:"front"`
-	Back  string `csv:"back"`
-	Hint  string `csv:"hint"`
+	Front 	string `csv:"front"`
+	Back  	string `csv:"back"`
+	Hint  	string `csv:"hint"`
+	Flipped	bool
+}
+
+//Flip tracks the flipped state of the card
+func (c *Card) Flip(){
+	if c.Flipped {
+		c.Flipped = false
+	}else{
+		c.Flipped = true
+	}
 }
--- cards/templates.go
+++ cards/templates.go
@@ -16,12 +16,20 @@ func CreateTemplate(name, words string) *template.Template {
 }
 
 //TemplateString used to create the correct card template output
-func TemplateString(c Card, face string) *template.Template {
+func TemplateString(c *Card, face string) *template.Template {
 	var result *template.Template
 	if strings.Compare(face, "hint") == 0 {
 		result = CreateTemplate("test3", CARDHINT)
 	} else if strings.Compare(face, "flip") == 0 {
-		result = CreateTemplate("test4", CARDBACK)
+
+		//Checks the flipped state and flips accordingly
+		if c.Flipped == true {
+			result = CreateTemplate("test5", CARDFRONT)
+		}else{
+			result = CreateTemplate("test4", CARDBACK)
+			}
+		//Change the flip state
+		c.Flip()
 	} else {
 		result = CreateTemplate("test5", CARDFRONT)
 	}
