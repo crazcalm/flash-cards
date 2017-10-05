614cb8cb3dacea77ef012137ce5e6af7972fd80f
Marcus Willock <marcuswillock@qq.com>
2017-10-06 01:05:06 +0800

    Added a test and turned another method into an internal method

--- cards/templates.go
+++ cards/templates.go
@@ -6,8 +6,8 @@ import (
 	"text/template"
 )
 
-//CreateTemplate is responsible for creating the templates
-func CreateTemplate(name, words string) *template.Template {
+//createTemplate is responsible for creating the templates
+func createTemplate(name, words string) *template.Template {
 	templ, err := template.New(name).Parse(words)
 	if err != nil {
 		log.Fatal(err)
@@ -19,19 +19,19 @@ func CreateTemplate(name, words string) *template.Template {
 func TemplateString(c *Card, face string) *template.Template {
 	var result *template.Template
 	if strings.Compare(face, "hint") == 0 {
-		result = CreateTemplate("test3", CARDHINT)
+		result = createTemplate("test3", CARDHINT)
 	} else if strings.Compare(face, "flip") == 0 {
 
 		//Checks the flipped state and flips accordingly
 		if c.Flipped == true {
-			result = CreateTemplate("test5", CARDFRONT)
+			result = createTemplate("test5", CARDFRONT)
 		} else {
-			result = CreateTemplate("test4", CARDBACK)
+			result = createTemplate("test4", CARDBACK)
 		}
 		//Change the flip state
 		c.Flip()
 	} else {
-		result = CreateTemplate("test5", CARDFRONT)
+		result = createTemplate("test5", CARDFRONT)
 	}
 	return result
 }
--- cards/utils_test.go
+++ cards/utils_test.go
@@ -7,6 +7,26 @@ import (
 	"text/template"
 )
 
+func TestInSlice(t *testing.T) {
+	//Test cases
+	var tests = []struct {
+		List   []string
+		Item   string
+		Answer bool
+	}{
+		{[]string{"yes", "no", "maybe"}, "yes", true},
+		{[]string{"yes", "no", "maybe"}, "idk", false},
+		{[]string{}, "", false},
+	}
+
+	for _, test := range tests {
+		result := InSlice(test.List, test.Item)
+		if result != test.Answer {
+			t.Fail()
+		}
+	}
+}
+
 func TestPrintToScreen(t *testing.T) {
 	// testing template
 	templ, err := template.New("testing1").Parse("{{.Front}} {{.Back}} {{.Hint}}")
